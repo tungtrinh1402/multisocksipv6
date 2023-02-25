98af64a3bbd2458b71faed04489998f8116a875f
tungtrinh1402 <tungtrinh1402@gmail.com>
2023-02-26 04:04:49 +0700

    1
    1

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ auth strong
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 $(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"socks5 -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
 }
